f0cc495c1bf96f0d783205246b3f7e820f9f959f
Shantanu Parab <112659509+shantanuparabumd@users.noreply.github.com>
2023-06-04 18:44:39 -0400

    Task 1.6.3: Walking Algorithm Gazebo

---
+++ Matlab Scripts/back_right.m
@@ -0,0 +1,12 @@
+% Create the robot model
+Ld0 = Link('d', 0, 'a', 0, 'alpha', pi/2);
+L0 = Link('d', 0, 'a', 0, 'alpha', 0);
+Ld1 = Link('d', 0.06485, 'a', 0, 'alpha', -pi/2);
+L1 = Link('d', 0.14, 'a', 0, 'alpha', 0);
+Ld2 = Link('d', 0, 'a', 0.5, 'alpha', 0);
+L2 = Link('d', 0, 'a', 0, 'alpha', 0);
+L3 = Link('d', 0, 'a', 0.45, 'alpha', 0);
+
+robot = SerialLink([Ld0 L0 Ld1 L1 Ld2 L2 L3]);
+robot.teach
+% Define the joint angles
